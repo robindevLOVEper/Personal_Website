793b0b772bf21007979b061238e8772871bbcdcd
Robin <133253788+robindevLOVEper@users.noreply.github.com>
2025-02-13 21:31:29 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#000000',
+        secondary: '#6B7280',
+      },
+    },
+  },
+  plugins: [],
+};
